8f3d71b1707c25baca2e83538a8e5f78a56738d5
MinjaVizi <viziminja1509@gmail.com>
2023-07-25 22:57:12 +0200

    dodata komponenta zadaci

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
+import React from 'react';
 import './App.css';
-import Navbar from './komponente/Navbar'; 
-import Pocetna from './komponente/Pocetna'; 
-
+import Navbar from './komponente/Navbar';
+import Pocetna from './komponente/Pocetna';
 
 function App() {
   return (
     <div className="App">
       <header>
+        {/* Sadržaj za header ako je potrebno */}
       </header>
 
-      <body> 
+      <body>
         <nav>
-
-          <Navbar /> 
-        <Pocetna/>
+          <Navbar ></Navbar>
+            <Pocetna /> {/* Ovdje se ubacuje komponenta Pocetna unutar Navbar */}
+          
         </nav>
-       
-
-
-
       </body>
     </div>
   );
 }
 
 export default App;
+
 
